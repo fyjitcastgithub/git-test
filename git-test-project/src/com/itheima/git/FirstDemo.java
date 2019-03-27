a859c9168cdf3c719edaa7c6ee85c9b5c4e49e69
itheima <itheima@itcast.cn>
2019-03-27 16:39:57 +0800

    添加了一个test方法后提交

--- git-test-project/src/com/itheima/git/FirstDemo.java
+++ git-test-project/src/com/itheima/git/FirstDemo.java
@@ -10,4 +10,9 @@ public class FirstDemo {
         System.out.println("Java从入门到放弃");
         System.out.println("Java从入门到放弃");
     }
+
+    public void test(){
+        int i=0;
+        i += 10;
+    }
 }
