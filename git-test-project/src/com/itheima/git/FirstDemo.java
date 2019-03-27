67a13a31659172d7c528884a2f9ab6f107b47b8c
itheima <itheima@itcast.cn>
2019-03-27 17:03:28 +0800

    添加了一个输出i的值的语句

--- git-test-project/src/com/itheima/git/FirstDemo.java
+++ git-test-project/src/com/itheima/git/FirstDemo.java
@@ -9,10 +9,13 @@ public class FirstDemo {
         System.out.println("Java从入门到放弃");
         System.out.println("Java从入门到放弃");
         System.out.println("Java从入门到放弃");
+        //添加注释
     }
 
     public void test(){
         int i=0;
         i += 10;
+        System.out.println(i);
+
     }
 }
