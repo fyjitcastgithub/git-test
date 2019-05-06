9d198d482b21d4309fb7aa4bc4c57cd29e8674f3
itheima <itheima@itcast.cn>
2019-05-07 00:43:51 +0800

    添加了一个Second类

---
+++ git-test-project/src/com/itheima/git/SecondTest.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+/**
+ * @author fyj
+ */
+public class SecondTest {
+
+    public static void main(String[] args) {
+        System.out.println("=============>");
+    }
+}
