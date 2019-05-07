fec665b79247b70b9d1635fd3f57113f07b40c71
itheima <itheima@itcast.cn>
2019-05-07 16:04:42 +0800

    在FirstDemo文件中添加信息后提交

--- git-test-project/src/com/itheima/git/FirstDemo.java
+++ git-test-project/src/com/itheima/git/FirstDemo.java
@@ -9,8 +9,9 @@ public class FirstDemo {
         System.out.println("Java从入门到放弃");
         System.out.println("Java从入门到放弃");
         System.out.println("Java从入门到放弃");
+
         //添加注释
-        System.out.println("杰哥，唱的不错啊！爱你。。。");
+        System.out.println("杰哥，唱的不错啊！呜呜呜呜 爱你。。。");
     }
 
     public void test(){
